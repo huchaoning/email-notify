9835cd6eb4fd201069cb4a1a393d591b383ff857
vxyi <vxyi@qq.com>
2025-06-08 22:58:30 +0800

    v1.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='email_notify',
     version='1.0.2',
     description='Email notifications when a code block or function finishes or ends unexpectedly',
-    long_description='',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Chao-Ning Hu',
     packages=find_packages(),
